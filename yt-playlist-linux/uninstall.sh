aa177b7514a1c7baba3961228e010012e9bd9a25
darnrain <60840489+darnrain@users.noreply.github.com>
2023-08-29 06:21:51 -0700

    Add files via upload
    yt-playlist linux

---
+++ yt-playlist-linux/uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+     clear
+     echo "yt-playlist v1.0 uninstaller"
+     sleep 0.8
+     sudo rm -v /usr/bin/yt-playlist
+     rm -v ${HOME}/.config/yt-playlist/input.conf
+     echo "${HOME}/.config/yt-playlist/ not removed"
+     echo "Your favorites were not deleted."
+     echo "Your history playlists were not deleted"
+     echo "yt-playlist uninstalled"
+
+exit
+
