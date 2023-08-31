c1b51ac6f453e0c11df6be76233bdbf30d7e62e6
darnrain <60840489+darnrain@users.noreply.github.com>
2023-08-31 06:02:52 -0700

    Update uninstall.sh

--- yt-playlist-linux/uninstall.sh
+++ yt-playlist-linux/uninstall.sh
@@ -3,7 +3,8 @@
      echo "yt-playlist-linux uninstaller"
      sleep 0.8
      sudo rm -v /usr/bin/yt-playlist
-     yes|rm -v -R /tmp/yt-playlist/
+     yes|rm -R /tmp/yt-playlist/
+     echo "removed '/tmp/yt-playlist'"
      rm -v ${HOME}/.config/yt-playlist/input.conf
      echo "${HOME}/.config/yt-playlist/ directory was not deleted"
      echo "Your history and favorites were not deleted"
