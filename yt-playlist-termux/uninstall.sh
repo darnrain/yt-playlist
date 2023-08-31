eb41b453012294ce765b1021ab0682dee719d587
darnrain <60840489+darnrain@users.noreply.github.com>
2023-08-31 07:17:15 -0700

    Update uninstall.sh

--- yt-playlist-termux/uninstall.sh
+++ yt-playlist-termux/uninstall.sh
@@ -2,7 +2,7 @@
 
      if command -v yt-playlist == *yt-playlist >/dev/null
      then
-     echo "yt-playlist-termux uninstaller"
+     echo "yt-playlist Termux uninstaller"
      sleep 0.8
      rm -v /data/data/com.termux/files/usr/bin/yt-playlist
      rm -v ${HOME}/.config/yt-playlist/input.conf
