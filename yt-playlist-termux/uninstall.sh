b370db3227b1938e47fe50838a97826e3520ddd5
darnrain <60840489+darnrain@users.noreply.github.com>
2023-08-31 04:48:49 -0700

    Update uninstall.sh

--- yt-playlist-termux/uninstall.sh
+++ yt-playlist-termux/uninstall.sh
@@ -6,8 +6,7 @@
      rm -v ${HOME}/.config/yt-playlist/input.conf
      yes|rm -R ${HOME}/yt-playlist/
      echo "~/.config/yt-playlist/ was not removed"
-     echo "Your history playlists were not deleted"
-     echo "Your favorites were not deleted."
+     echo "Your history and favorites were not deleted"
      echo "yt-playlist uninstalled"
 
 exit
