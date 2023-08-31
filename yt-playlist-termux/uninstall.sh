811a8dc9b5b30c1c4f6cc570dfa8e16f57f4177c
darnrain <60840489+darnrain@users.noreply.github.com>
2023-08-31 05:54:29 -0700

    Update uninstall.sh

--- yt-playlist-termux/uninstall.sh
+++ yt-playlist-termux/uninstall.sh
@@ -5,7 +5,7 @@
      rm -v /data/data/com.termux/files/usr/bin/yt-playlist
      rm -v ${HOME}/.config/yt-playlist/input.conf
      yes|rm -R /data/data/com.termux/files/usr/tmp/yt-playlist/
-     echo "/data/data/com.termux/files/usr/tmp/yt-playlist/ tmp directory deleted."
+     echo "removed '/data/data/com.termux/files/usr/tmp/yt-playlist/'"
      echo "${HOME}/.config/yt-playlist/ directory was not deleted"
      echo "Your history and favorites were not deleted"
      echo "yt-playlist uninstalled"
