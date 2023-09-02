2ddd7743063e6aa5d421a52ebf43a9bd1b872cfa
darnrain <60840489+darnrain@users.noreply.github.com>
2023-09-01 21:25:38 -0700

    Add files via upload

---
+++ yt-playlist-termux/uninstall.sh
@@ -0,0 +1,17 @@
+#!/data/data/com.termux/files/usr/bin/bash 
+
+     	if command -v yt-playlist == *yt-playlist >/dev/null
+     	then
+     	echo "yt-playlist Termux uninstaller"
+     	sleep 0.8
+     	rm -v /data/data/com.termux/files/usr/bin/yt-playlist
+     	rm -v ${HOME}/.config/yt-playlist/input.conf
+     	echo "Not removed '${HOME}/.config/yt-playlist/'"
+     	echo "Bookmarks, Music, Favorites and History were not deleted."
+     	echo "yt-playlist uninstalled."
+     	exit
+     	else
+     	echo "Error uninstalling yt-playlist."
+     	echo "yt-playlist is not installed."
+     	exit
+	fi
