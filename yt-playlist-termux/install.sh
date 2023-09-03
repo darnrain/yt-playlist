2b302d919e6bc412f826ec4076cb7fbc14fdb65a
darnrain <60840489+darnrain@users.noreply.github.com>
2023-09-03 07:04:06 -0700

    Add files via upload
    Updated the Termux installer to auto install the missing dependencies

--- yt-playlist-termux/install.sh
+++ yt-playlist-termux/install.sh
@@ -1,7 +1,7 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
         
-        if command -v fzf == *fzf >/dev/null && command -v mpv == *mpv >/dev/null && command -v yt-dlp == *yt-dlp >/dev/null && command -v socat == *socat >/dev/null && command -v awk == *awk >/dev/null && command -v sed == *sed >/dev/null && command -v tr == *tr >/dev/null
+        if command -v fzf == *fzf >/dev/null && command -v mpv == *mpv >/dev/null && command -v yt-dlp == *yt-dlp >/dev/null && command -v socat == *socat >/dev/null
         then
         pkill yt-playlist
         pkill mpv
@@ -26,17 +26,37 @@
         else
         clear
         echo "Not all dependencies were found."
-        echo "yt-playlist Termux dependencies are"
-        echo "fzf,mpv,yt-dlp,socat,awk,sed,tr"
-        echo ""
-        command -V fzf
-        command -V mpv
-        command -V yt-dlp
-        command -V socat
-        command -V awk
-        command -V sed
-        command -V tr
-        echo ""
-        echo "Please install the missing dependencies and run install again."
+        echo "Installing missing dependencies."
+        echo "In ten seconds."
+        echo "ctrl-c to cancel"
+        sleep 10
+        yes|pkg update
+        yes|pkg upgrade
+        yes|pkg install fzf
+        yes|pkg install mpv
+        yes|pkg install socat
+        yes|pkg install python-pip
+        yes|pip install yt-dlp
+        clear
+        pkill yt-playlist
+        pkill mpv
+        echo "yt-playlist Termux installer."
+        echo "All dependencies were found."
+        cp ./yt-playlist /data/data/com.termux/files/usr/bin/
+        #cp /data/data/com.termux/files/usr/tmp/yt-playlist/yt-playlist-termux/yt-playlist /data/data/com.termux/files/usr/bin/
+        chmod +x /data/data/com.termux/files/usr/bin/yt-playlist
+        mkdir -p ${HOME}/.config/yt-playlist/
+        touch ${HOME}/.config/yt-playlist/yt-playlist_favorites.txt
+        touch ${HOME}/.config/yt-playlist/yt-playlist_search_history.txt
+        touch ${HOME}/.config/yt-playlist/yt-playlist_bookmark.txt
+        touch ${HOME}/.config/yt-playlist/yt-playlist_music.txt
+        touch ${HOME}/.config/yt-playlist/input.conf
+        echo "d run /data/data/com.termux/files/usr/bin/yt-playlist --download_audio" > ${HOME}/.config/yt-playlist/input.conf
+        echo "D run /data/data/com.termux/files/usr/bin/yt-playlist --download_video" >> ${HOME}/.config/yt-playlist/input.conf
+        echo "s run /data/data/com.termux/files/usr/bin/yt-playlist --sleep_time" >> ${HOME}/.config/yt-playlist/input.conf
+        echo "b run /data/data/com.termux/files/usr/bin/yt-playlist --bookmark" >> ${HOME}/.config/yt-playlist/input.conf
+        echo "f run /data/data/com.termux/files/usr/bin/yt-playlist --favorites" >> ${HOME}/.config/yt-playlist/input.conf
+        echo "t run /data/data/com.termux/files/usr/bin/yt-playlist --date_time" >> ${HOME}/.config/yt-playlist/input.conf
+        echo 'Type "yt-playlist" to run program.'
         exit
         fi
