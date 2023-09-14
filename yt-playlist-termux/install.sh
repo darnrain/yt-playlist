825782ca64baf8896bc72003c95bfd00cda82e18
darnrain <60840489+darnrain@users.noreply.github.com>
2023-09-14 09:23:58 -0700

    Add files via upload
    Found some issues

--- yt-playlist-termux/install.sh
+++ yt-playlist-termux/install.sh
@@ -10,8 +10,7 @@
         echo "============================"
         echo "All dependencies were found."
         echo "============================"
-        cp ./yt-playlist /data/data/com.termux/files/usr/bin/
-        #cp /data/data/com.termux/files/usr/tmp/yt-playlist/yt-playlist-termux/yt-playlist /data/data/com.termux/files/usr/bin/
+        cp /data/data/com.termux/files/usr/tmp/yt-playlist/yt-playlist-termux/yt-playlist /data/data/com.termux/files/usr/bin/
         chmod +x /data/data/com.termux/files/usr/bin/yt-playlist
         mkdir -p ${HOME}/.config/yt-playlist/
         touch ${HOME}/.config/yt-playlist/yt-playlist_favorites.txt
