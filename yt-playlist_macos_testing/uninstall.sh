25a15419bb92d3281f5b589f54cfb87f4e4ba1a0
darnrain <60840489+darnrain@users.noreply.github.com>
2023-09-18 00:56:17 -0700

    Add files via upload

---
+++ yt-playlist_macos_testing/uninstall.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+     	if command -v yt-playlist == *yt-playlist >/dev/null
+     	then
+        echo "============================"
+     	echo "Linux uninstaller yt-playlist"
+        echo "============================"
+     	sudo rm -v /usr/bin/yt-playlist
+     	rm -v ${HOME}/.config/yt-playlist/input.conf
+     	echo "Not removed '${HOME}/.config/yt-playlist/'"
+     	echo "Bookmarks, Music, Favorites and History were not deleted."
+        echo "============================"
+     	echo "yt-playlist uninstalled."
+        echo "============================"
+     	exit
+     	else
+        echo "============================"
+     	echo "Error uninstalling yt-playlist."
+     	echo "yt-playlist is not installed."
+        echo "============================"
+     	exit
+     	fi
