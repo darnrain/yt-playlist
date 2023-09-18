3d7265b7c66c8b9b4027e469c4dfd3e676387174
darnrain <60840489+darnrain@users.noreply.github.com>
2023-09-18 04:31:29 -0700

    Update uninstall.sh

--- yt-playlist_macos_beta/uninstall.sh
+++ yt-playlist_macos_beta/uninstall.sh
@@ -5,7 +5,7 @@
         echo "============================"
      	echo "Linux uninstaller yt-playlist"
         echo "============================"
-     	sudo rm -v /usr/bin/yt-playlist
+     	sudo rm -v /usr/local/bin/yt-playlist
      	rm -v ${HOME}/.config/yt-playlist/input.conf
      	echo "Not removed '${HOME}/.config/yt-playlist/'"
      	echo "Bookmarks, Music, Favorites and History were not deleted."
