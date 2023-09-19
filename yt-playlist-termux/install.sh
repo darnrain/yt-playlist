af86d3203f789c9726ccf7e2d2d20f2dbb0e4049
darnrain <60840489+darnrain@users.noreply.github.com>
2023-09-19 10:11:54 -0700

    Add files via upload
    Updated termux install.sh

--- yt-playlist-termux/install.sh
+++ yt-playlist-termux/install.sh
@@ -2,17 +2,8 @@
 
         externel_argument="$1"
 
-        if ! pgrep yt-playlist >/dev/null
-        then
-        if ! pgrep mpv >/dev/null
-        then
-        if command -v yt-dlp >/dev/null && command -v mpv >/dev/null && command -v ffmpeg >/dev/null && command -v fzf >/dev/null && command -v mutagen-inspect >/dev/null && command -v socat >/dev/null && command -v fx >/dev/null && command -v wget >/dev/null && command -v curl >/dev/null
-        then
-        echo "============================"
-        echo "Termux installer yt-playlist."
-        echo "============================"
-        echo "All dependencies were found."
-        echo "============================"
+function termux_install_function ()
+{
         if [[ "$externel_argument" = "--online_install" ]]
 	then
         cp ${TMPDIR}/yt-playlist/yt-playlist-termux/yt-playlist /data/data/com.termux/files/usr/bin/
@@ -40,10 +31,24 @@
         echo "T run /data/data/com.termux/files/usr/bin/yt-playlist --title" >> ${HOME}/.config/yt-playlist/input.conf
         sed -i '/yt-playlist/d' ${HOME}/.bashrc
         echo "alias y='yt-playlist'" >> ${HOME}/.bashrc
-        sed -i '/# volume-keys/s/^#//' /data/data/com.termux/files/home/.termux/termux.properties
+        sed -i'' '/# volume-keys/s/^#//' /data/data/com.termux/files/home/.termux/termux.properties
         echo 'Please type "exit" to close Termux.'
         echo 'Then type "y" to run yt-playlist'
         echo "============================"
+}
+
+        if ! pgrep yt-playlist >/dev/null
+        then
+        if ! pgrep mpv >/dev/null
+        then
+        if command -v yt-dlp >/dev/null && command -v mpv >/dev/null && command -v ffmpeg >/dev/null && command -v fzf >/dev/null && command -v mutagen-inspect >/dev/null && command -v socat >/dev/null && command -v fx >/dev/null && command -v wget >/dev/null && command -v curl >/dev/null
+        then
+        echo "============================"
+        echo "Termux installer yt-playlist."
+        echo "============================"
+        echo "All dependencies were found."
+        echo "============================"
+        termux_install_function 
         exit
         else
         clear
@@ -76,28 +81,7 @@
         echo "============================"
         echo "All dependencies are installed."
         echo "============================"
-        cp /data/data/com.termux/files/usr/tmp/yt-playlist/yt-playlist-termux/yt-playlist /data/data/com.termux/files/usr/bin/
-        chmod +x /data/data/com.termux/files/usr/bin/yt-playlist
-        mkdir -p ${HOME}/.config/yt-playlist/
-        mkdir -p ${HOME}/.config/yt-playlist/scripts/
-        mkdir -p ${HOME}/.config/yt-playlist/scripts/sponsorblock_shared/
-        curl -s https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock.lua -L -o ${HOME}/.config/yt-playlist/scripts/sponsorblock.lua
-        curl -s https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock_shared/main.lua -L -o ${HOME}/.config/yt-playlist/scripts/sponsorblock_shared/main.lua
-        curl -s https://raw.githubusercontent.com/po5/mpv_sponsorblock/master/sponsorblock_shared/sponsorblock.py -L -o ${HOME}/.config/yt-playlist/scripts/sponsorblock_shared/sponsorblock.py
-        touch ${HOME}/.config/yt-playlist/yt-playlist_favorites.txt
-        touch ${HOME}/.config/yt-playlist/yt-playlist_search_history.txt
-        touch ${HOME}/.config/yt-playlist/yt-playlist_bookmark.txt
-        touch ${HOME}/.config/yt-playlist/yt-playlist_music.txt
-        touch ${HOME}/.config/yt-playlist/input.conf
-        echo "d run /data/data/com.termux/files/usr/bin/yt-playlist --download_audio" > ${HOME}/.config/yt-playlist/input.conf
-        echo "D run /data/data/com.termux/files/usr/bin/yt-playlist --download_video" >> ${HOME}/.config/yt-playlist/input.conf
-        echo "s run /data/data/com.termux/files/usr/bin/yt-playlist --sleep_time" >> ${HOME}/.config/yt-playlist/input.conf
-        echo "b run /data/data/com.termux/files/usr/bin/yt-playlist --bookmark" >> ${HOME}/.config/yt-playlist/input.conf
-        echo "f run /data/data/com.termux/files/usr/bin/yt-playlist --favorites" >> ${HOME}/.config/yt-playlist/input.conf
-        echo "t run /data/data/com.termux/files/usr/bin/yt-playlist --date_time" >> ${HOME}/.config/yt-playlist/input.conf
-        echo 'Please type "exit" to close Termux.'
-        echo 'Then type "y" to run yt-playlist'
-        echo "============================"
+        termux_install_function 
         exit
         fi
         else
