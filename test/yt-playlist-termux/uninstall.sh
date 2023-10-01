2de9dbac0b1882e4030f0c875bbbf737b4d671ae
darnrain <60840489+darnrain@users.noreply.github.com>
2023-10-01 06:07:01 -0700

    Add files via upload
    Changed the format of the code in yt-playlist, install.sh and uninstall.sh
    Fixed the Playlist menu number 4.

--- test/yt-playlist-termux/uninstall.sh
+++ test/yt-playlist-termux/uninstall.sh
@@ -2,28 +2,27 @@
 
 	if [ "$EUID" == "0" ]
 	then echo "Please do not run as root."
-  	exit
+	exit
 	fi
-
-     	if command -v yt-playlist >/dev/null
-     	then
-        echo "============================"
-     	echo "Termux uninstaller yt-playlist"
-        echo "============================"
-     	rm -v /data/data/com.termux/files/usr/bin/yt-playlist
-     	rm -v ${HOME}/.config/yt-playlist/input.conf
-     	rm -R -v ${HOME}/.config/yt-playlist/scripts/
-        sed -i '/yt-playlist/d' ${HOME}/.bashrc
-     	echo "Not removed '${HOME}/.config/yt-playlist/'"
-     	echo "Bookmarks, Music, Favorites and History were not deleted."
-        echo "============================"
-     	echo "yt-playlist uninstalled."
-        echo "============================"
-     	exit
-     	else
-        echo "============================"
-     	echo "Error uninstalling yt-playlist."
-     	echo "yt-playlist is not installed."
-        echo "============================"
-     	exit
+	if command -v yt-playlist >/dev/null
+	then
+	echo "============================"
+	echo "Termux uninstaller yt-playlist"
+	echo "============================"
+	rm -v /data/data/com.termux/files/usr/bin/yt-playlist
+	rm -v ${HOME}/.config/yt-playlist/input.conf
+	rm -R -v ${HOME}/.config/yt-playlist/scripts/
+	sed -i '/yt-playlist/d' ${HOME}/.bashrc
+	echo "Not removed '${HOME}/.config/yt-playlist/'"
+	echo "Bookmarks, Music, Favorites and History were not deleted."
+	echo "============================"
+	echo "yt-playlist uninstalled."
+	echo "============================"
+	exit
+	else
+	echo "============================"
+	echo "Error uninstalling yt-playlist."
+	echo "yt-playlist is not installed."
+	echo "============================"
+	exit
 	fi
